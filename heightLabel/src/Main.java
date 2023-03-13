a0bcaf414bc1f1fb4b6abc6cae548563e1a54ad5
Sidney Kaguli <developer.sid@outlook.com>
2023-03-13 11:46:36 +0300

    height label sol

---
+++ heightLabel/src/Main.java
@@ -0,0 +1,22 @@
+public class HeightLabel {
+    
+    public TreeNode rewire(TreeNode t) {
+        if (t == null) {
+            return null;
+        }
+        int height = getHeight(t);
+        TreeNode node = new TreeNode(height);
+        node.left = rewire(t.left);
+        node.right = rewire(t.right);
+        return node;
+    }
+    
+    private int getHeight(TreeNode t) {
+        if (t == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(t.left);
+        int rightHeight = getHeight(t.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
