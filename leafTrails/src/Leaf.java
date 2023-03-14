00533dc42218007e07a5540c792c6e212af26acc
Sidney Kaguli <developer.sid@outlook.com>
2023-03-14 23:29:01 +0300

    leaf trail sol

---
+++ leafTrails/src/Leaf.java
@@ -0,0 +1,13 @@
+public class Leaf {
+    int val;
+    String path;
+
+    Leaf(int val, String path) {
+        this.val = val;
+        this.path = path;
+    }
+
+    public int getVal(){
+        return val;
+    }
+}
